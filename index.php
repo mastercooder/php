c62c8eea277b80bff3350e0fbe51a326a34697ee
mastercooder <tejesh.patel256@gmail.com>
2024-11-28 15:49:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,65 @@
 </head>
 <body>
     <div class="container">
-        This is my first PHP program
+        <h1>Learning PHP</h1>
+
+        
         <?php
-            echo "Hello WOrld -> This is printed using PHP"
+            // Php code is here -> and this is how you write comment in php
+            echo "Hello WOrld -> This is printed using PHP";
+            echo "<br>";
+            echo "<br>";
+
+            echo "<b>Variable In PHP</b>";
+            echo "<br>";
+            $varable1 = 1;
+            $varable2 = 2;
+            echo $varable1;
+            echo $varable2;
+            echo "<br>";
+            echo $varable1 + $varable2;
+            // php is not Case Sensitive
+
+            echo "<br>";
+            echo "<br>";
+            echo "<b>Operators in PHP</b>";
+            echo "<br>";
+            echo "Arathmatic Operators: -, +, *, /";
+            echo "<br>";
+            echo "Assignment Operators: =, +=, -=, *=, /=";
+            echo "<br>";
+            echo "Comparision Operators: ==, >=, <=, !="; 
+            // var_dump() -> This is a function used to see the datatype of any variable
+            echo "<br>";
+            echo "Increment/Decrement Opeartors: ++, --";
+            echo "<br>";
+            echo "Logical Operator: and(&&), or(||), xor, not(!)";
+            echo "<br>";
+            echo "<br>";
+
+            echo "<b>Data Type</b><br>";
+            echo "String, Integer, Float, Boolean, Array, Object <br>";
+            echo "String";
+            $string = "This is a String";
+            var_dump($string);
+            echo "<br>";
+            echo "Integer: ";
+            $int = 10;
+            echo var_dump($int);
+            echo "<br>";
+            echo "Float: ";
+            $float = 55.6;
+            echo var_dump($float);
+            echo "<br>";
+            echo "Boolean: ";
+            $bol = true;
+            echo var_dump($bol);
+            echo "<br>";
+            echo "<br>";
+
+            echo "<b>Constant</b><br>";
+            define('PI', 3.14);
+            echo PI;
         ?>
     </div>
 </body>
